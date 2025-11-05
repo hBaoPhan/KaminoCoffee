4981dcad58e5c9abb62aa2050bf12f31e433d843
NotHoaiBao <baophan2929@gmail.com>
2025-11-05 12:41:40 +0700

    Update ConnectDB.java

--- src/connectDB/ConnectDB.java
+++ src/connectDB/ConnectDB.java
@@ -4,19 +4,32 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectDB {
+	public static Connection con=null;
+	private static ConnectDB instance=new ConnectDB();
 	
-	public static void connect() {
-		String url = "jdbc:sqlserver://localhost:1433;databaseName=QUANLYKAMINOCOFFEE;encrypt=true;trustServerCertificate=true";
-        String user = "sa";
-        String password = "sapassword";
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Connected successfully!");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+	public static Connection getConnection() {
+		return con;	
 	}
-	public static void main(String[] args) {
+	
+	public static ConnectDB getInstance() {
+		 return instance;
+	}
+	
+	public void connect() throws SQLException {
+		String url="jdbc:sqlserver://localhost:1433;databasename=QUANLYKAMINOCOFFEE";
+		String user="sa";
+		String password="sapassword";
+		con=DriverManager.getConnection(url,user,password);
+	}
+	public void disconnnect() {
+		 if(con !=null) {
+			 try {
+				con.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		 }
 		
-    }
-
+	}
+	 
 }
